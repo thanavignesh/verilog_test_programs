8b5aba0eaa6d19f9990ad63f9506313c5722239c
thanavignesh <83361794+thanavignesh@users.noreply.github.com>
2023-02-27 11:40:00 +0530

    Create and_tb.v

---
+++ prob_1/and_tb.v
@@ -0,0 +1,19 @@
+module and_tb;
+
+reg a;
+reg b;
+wire c;
+
+  and_gat dut(.a(a),.b(b),.c(c));
+
+initial 
+begin
+	a = 1'b0;
+	b = 1'b0;
+  #5 {a,b} = 2'b11;
+  #5 {a,b} = 2'b10;
+end
+
+initial #20 $finish;
+
+endmodule
