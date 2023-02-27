9d2f274d2129bfd50d55432d14a5e36b1b447864
thanavignesh <83361794+thanavignesh@users.noreply.github.com>
2023-02-27 12:15:46 +0530

    Create left_shifter.v

---
+++ prob_8/left_shifter.v
@@ -0,0 +1,14 @@
+module left_shifter(output reg[3:0]Q,
+input clk,reset
+);
+reg [3:0]d=3'b001;
+always@(posedge clk or reset )
+begin
+if(reset)
+Q <= d;
+else begin
+Q <= Q<<1;
+Q[0] <= Q[3];
+end 
+end 
+endmodule
